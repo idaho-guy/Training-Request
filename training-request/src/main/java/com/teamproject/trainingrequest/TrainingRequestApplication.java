ef0ccb05267fe6b18cdd0822aace05ac349034b0
Ceasar Barbosa <cbarbosa@solstice.com>
2018-07-27 15:59:07 -0500

    Eureka Configuration

--- training-request/src/main/java/com/teamproject/trainingrequest/TrainingRequestApplication.java
+++ training-request/src/main/java/com/teamproject/trainingrequest/TrainingRequestApplication.java
@@ -2,10 +2,12 @@ package com.teamproject.trainingrequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class TrainingRequestApplication {
 
 	public static void main(String[] args) {
--- training-request/src/main/java/com/teamproject/trainingrequest/service/EmployeeService.java
+++ training-request/src/main/java/com/teamproject/trainingrequest/service/EmployeeService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "employee-service", url = "http://localhost:8080")
+@FeignClient(name = "employee-service")
 public interface EmployeeService {
 
     @GetMapping("/employees/{id}")
