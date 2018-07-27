9cef7496617708d953849a32bb59dcbb686260a4
Ceasar Barbosa <cbarbosa@solstice.com>
2018-07-27 16:39:24 -0500

    Implement Zuul

--- api-gateway/src/main/java/com/teamproject/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/teamproject/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,17 @@ package com.teamproject.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
 }
