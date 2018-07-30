5a38742aab659e3e22d4d3fe5fcb3a4dfbc925c5
Ceasar Barbosa <cbarbosa@solstice.com>
2018-07-30 10:38:51 -0500

    Addressed Swagger breaking tests

--- training-request/src/main/java/com/teamproject/trainingrequest/documentation/SwaggerConfig.java
+++ training-request/src/main/java/com/teamproject/trainingrequest/documentation/SwaggerConfig.java
@@ -2,15 +2,17 @@ package com.teamproject.trainingrequest.documentation;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     public final HashSet<String> PRODUCES_AND_CONSUME = new HashSet<>(Arrays.asList("application/json", "application/xml"));
