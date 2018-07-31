0c6a70f189b8651ccc35803b45ac5a0c49e126f8
Ceasar Barbosa <cbarbosa@solstice.com>
2018-07-31 11:06:42 -0500

    Added Postman collection json

--- employee/src/main/java/com/teamproject/employee/exception/EmployeeNotFoundException.java
+++ employee/src/main/java/com/teamproject/employee/exception/EmployeeNotFoundException.java
@@ -3,7 +3,7 @@ package com.teamproject.employee.exception;
 public class EmployeeNotFoundException extends RuntimeException {
 
     public EmployeeNotFoundException(Long employeeId) {
-        super(String.format("Employee with id %d not foune", employeeId));
+        super(String.format("Employee with id %d not found", employeeId));
 
     }
 }
